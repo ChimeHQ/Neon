697609270cc370bb01f6bacb8d5624ac921bf332
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-03-27 13:14:28 -0400

    iOS support

--- Package.swift
+++ Package.swift
@@ -4,21 +4,17 @@ import PackageDescription
 
 let package = Package(
     name: "Neon",
-    platforms: [.macOS(.v10_13)],
+    platforms: [.macOS(.v10_13), .iOS(.v11)],
     products: [
         .library(name: "Neon", targets: ["Neon"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.4.0"),
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.4.1"),
         .package(url: "https://github.com/ChimeHQ/Rearrange", from: "1.5.1"),
         .package(url: "https://github.com/ChimeHQ/OperationPlus", from: "1.5.4"),
     ],
     targets: [
         .target(name: "Neon", dependencies: ["SwiftTreeSitter", "Rearrange", "OperationPlus"]),
-        .testTarget(
-            name: "NeonTests",
-            dependencies: ["Neon"],
-            linkerSettings: [.linkedLibrary("c++")]
-        ),
+        .testTarget(name: "NeonTests", dependencies: ["Neon"]),
     ]
 )
