7cdb0e77f0d02cf3c58950d50178c7b65174be28
Kaunteya Suryawanshi <k.suryawanshi@gmail.com>
2022-08-27 18:09:08 +0530

    Updated the code to handle the nil return from transformer

--- Sources/TreeSitterClient/TreeSitter+Extensions.swift
+++ Sources/TreeSitterClient/TreeSitter+Extensions.swift
@@ -6,7 +6,7 @@ extension Point {
 }
 
 extension InputEdit {
-    init?(range: NSRange, delta: Int, oldEndPoint: Point = .zero, transformer: Point.LocationTransformer? = nil) {
+    init?(range: NSRange, delta: Int, oldEndPoint: Point, transformer: Point.LocationTransformer? = nil) {
         let startLocation = range.location
         let newEndLocation = range.max + delta
 
@@ -15,14 +15,25 @@ extension InputEdit {
             return nil
         }
 
-        let startPoint = transformer?(startLocation) ?? .zero
-        let newEndPoint = transformer?(newEndLocation) ?? .zero
+        let startPoint, newEndPoint: Point?
+        if let transformer = transformer {
+            startPoint = transformer(startLocation)
+            newEndPoint = transformer(newEndLocation)
+            if startPoint == nil || newEndPoint == nil {
+                return nil
+            }
+        } else {
+            startPoint = .zero
+            newEndPoint = .zero
+        }
 
+        assert(startPoint != nil, "startPoint should not be nil")
+        assert(newEndPoint != nil, "newEndPoint should not be nil")
         self.init(startByte: UInt32(range.location * 2),
                   oldEndByte: UInt32(range.max * 2),
                   newEndByte: UInt32(newEndLocation * 2),
-                  startPoint: startPoint,
+                  startPoint: startPoint!,
                   oldEndPoint: oldEndPoint,
-                  newEndPoint: newEndPoint)
+                  newEndPoint: newEndPoint!)
     }
 }
