77d2998bfb7dbaebfdb606bb660bb4bc572e3be2
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-01-21 07:15:32 -0500

    Remove this bad idea

--- Sources/RangeState/HybridValueProvider+RangeProcessor.swift
+++ Sources/RangeState/HybridValueProvider+RangeProcessor.swift
@@ -1,35 +1,6 @@
 import Foundation
 
 extension HybridValueProvider {
-	/// Don't use this
-	@MainActor
-	public init(
-		value: Output,
-		rangeProcessor: RangeProcessor,
-		inputTransformer: @escaping (Input) -> (Int, RangeFillMode)
-	) {
-		self.init(
-			syncValue: { input in
-				let (location, fill) = inputTransformer(input)
-
-				if rangeProcessor.processLocation(location, mode: fill) {
-					return value
-				}
-
-				return nil
-			},
-			asyncValue: { input in
-				let (location, fill) = inputTransformer(input)
-
-                rangeProcessor.processLocation(location, mode: fill)
-
-				await rangeProcessor.processingCompleted()
-
-				return value
-			}
-		)
-	}
-
 	/// Construct a `HybridValueProvider` that will first attempt to process a location using a `RangeProcessor`.
 	@MainActor
 	public init(
