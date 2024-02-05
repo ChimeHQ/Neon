656255fd03ccbb9ff3f3e54f04d11fe7e92e4fae
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-02-05 15:21:44 -0500

    Remove unused initializer

--- Sources/TreeSitterClient/TreeSitter+Extensions.swift
+++ Sources/TreeSitterClient/TreeSitter+Extensions.swift
@@ -4,31 +4,6 @@ import SwiftTreeSitter
 import SwiftTreeSitterLayer
 
 extension InputEdit {
-	init?(range: NSRange, delta: Int, oldEndPoint: Point, transformer: Point.LocationTransformer? = nil) {
-		let startLocation = range.location
-		let newEndLocation = range.max + delta
-
-		if newEndLocation < 0 {
-			assertionFailure("invalid range/delta")
-			return nil
-		}
-
-		let startPoint = transformer?(startLocation)
-		let newEndPoint = transformer?(newEndLocation)
-
-		if transformer != nil {
-			assert(startPoint != nil)
-			assert(newEndPoint != nil)
-		}
-
-		self.init(startByte: UInt32(range.location * 2),
-				  oldEndByte: UInt32(range.max * 2),
-				  newEndByte: UInt32(newEndLocation * 2),
-				  startPoint: startPoint ?? .zero,
-				  oldEndPoint: oldEndPoint,
-				  newEndPoint: newEndPoint ?? .zero)
-	}
-
 	init(range: NSRange, delta: Int, oldEndPoint: Point, transformer: Point.LocationTransformer) {
 		let startLocation = range.location
 		let newEndLocation = range.upperBound + delta
@@ -39,11 +14,13 @@ extension InputEdit {
 		let startPoint = transformer(startLocation) ?? .zero
 		let newEndPoint = transformer(newEndLocation) ?? .zero
 
-		self.init(startByte: UInt32(range.location * 2),
-				  oldEndByte: UInt32(range.upperBound * 2),
-				  newEndByte: UInt32(newEndLocation * 2),
-				  startPoint: startPoint,
-				  oldEndPoint: oldEndPoint,
-				  newEndPoint: newEndPoint)
+		self.init(
+			startByte: UInt32(range.location * 2),
+			oldEndByte: UInt32(range.upperBound * 2),
+			newEndByte: UInt32(newEndLocation * 2),
+			startPoint: startPoint,
+			oldEndPoint: oldEndPoint,
+			newEndPoint: newEndPoint
+		)
 	}
 }
