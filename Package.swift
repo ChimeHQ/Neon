8d01febc54d0bedba1136ef3bc833ddd7c843528
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-09-11 07:01:05 -0400

    Use latest SwiftTreeSitter

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 		.library(name: "Neon", targets: ["Neon"]),
 	],
 	dependencies: [
-		.package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", revision: "b01904a3737649c1d8520106bbb285724fe5b0bb"),
+		.package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", revision: "d0938d3607abcc5669bdf1d7d3b0485a8740fc65"),
 		.package(url: "https://github.com/ChimeHQ/Rearrange", from: "1.8.1"),
 	],
 	targets: [
