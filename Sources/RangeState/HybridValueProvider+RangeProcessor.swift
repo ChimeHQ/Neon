e6330773085ae43d6c33157c101bf652af713fdf
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-10-19 11:30:37 -0400

    Local function workaround needed to fix race

--- Sources/RangeState/HybridValueProvider+RangeProcessor.swift
+++ Sources/RangeState/HybridValueProvider+RangeProcessor.swift
@@ -10,25 +10,27 @@ extension HybridSyncAsyncValueProvider {
 		asyncValue: @escaping (Input) async throws(Failure) -> sending Output
 	) {
 		// bizarre local-function workaround https://github.com/swiftlang/swift/issues/77067
+		func _syncVersion(input: sending Input) throws(Failure) -> sending Output? {
+			let (location, fill) = inputTransformer(input)
+
+			if rangeProcessor.processLocation(isolation: isolation, location, mode: fill) {
+				return try syncValue(input)
+			}
+
+			return nil
+		}
+
 		func _asyncVersion(isolation: isolated(any Actor)?, input: sending Input) async throws(Failure) -> sending Output {
 			let (location, fill) = inputTransformer(input)
 
-			rangeProcessor.processLocation(location, mode: fill)
+			rangeProcessor.processLocation(isolation: isolation, location, mode: fill)
 			await rangeProcessor.processingCompleted()
 
 			return try await asyncValue(input)
 		}
 
 		self.init(
-			syncValue: { (input) throws(Failure) in
-				let (location, fill) = inputTransformer(input)
-
-				if rangeProcessor.processLocation(location, mode: fill) {
-					return try syncValue(input)
-				}
-
-				return nil
-			},
+			syncValue: _syncVersion,
 			asyncValue: _asyncVersion
 		)
 	}
